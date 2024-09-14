441140d7254dcb4c0c29ea1e03bb70f76a21cbda
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-14 11:26:39 +0100

    updated the algorithms ( findColor-func &&  CompareCombinations   ) and testing it

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,62 @@
 import React from "react";
+import "./App.css";
+
+const fibonacciNumbers = [1, 1, 2, 3, 5];
+
+const findColors = (arr1, arr2) => {
+  const result = [];
+  const allFibonacci = new Set(fibonacciNumbers);
+  const setArr2 = new Set(arr2);
+
+  allFibonacci.forEach((num) => {
+    const isInArr1 = arr1.includes(num);
+    const isInArr2 = arr2.includes(num);
+
+    if (isInArr1 && isInArr2) {
+      result.push({ value: num, color: "blue" });
+    } else if (isInArr1) {
+      result.push({ value: num, color: "red" });
+    } else if (isInArr2) {
+      result.push({ value: num, color: "green" });
+    } else {
+      result.push({ value: num, color: "white" });
+    }
+  });
+
+  return result;
+};
+
+const CompareCombinations = (objH, objM) => {
+  let results = [];
+
+  objH.forEach((arrH) => {
+    objM.forEach((arrM) => {
+      results.push(findColors(arrH, arrM));
+    });
+  });
+
+  return results;
+};
+
+// Example hour and minute combinations (replace with actual logic)
+const hourCombinations = [
+  [1, 1],
+  [2, 3],
+  [1, 5],
+];
+
+const minuteCombinations = [[1, 2, 3], [5], [1, 2, 5]];
 
 const App = () => {
-  let a = 0;
-  let b = 0;
-
-  const findColors = (arr1, arr2) => {
-    let result = [];
-    let Hcolors = [];
-    let Mcolors = [];
-    for (let item of arr1) {
-      for (let i of arr2) {
-        if (item === i) {
-          a++;
-        }
-      }
-      a === 0 ? Hcolors.push([item, "green"]) : Hcolors.push([item, "blue"]);
-      a = 0;
-    }
-
-    for (let item of arr2) {
-      for (let i of arr1) {
-        if (item === i) {
-          a++;
-        }
-      }
-      a === 0 ? Mcolors.push([item, "red"]) : Mcolors.push([item, "blue"]);
-      a = 0;
-    }
-    result.push(Hcolors, Mcolors);
-    return result;
-  };
-  console.log(findColors([1, 5, 2], [3, 1, 4]));
+  const results = CompareCombinations(hourCombinations, minuteCombinations);
 
   return (
-    <div className="App">
-      {findColors([1, 5, 2], [3, 1, 4]).map((iterator) => {
-        const { Hcolors, Mcolors } = iterator;
-        Hcolors?.map((row) => {
-          return (
-            <>
-              <p style={{ padding: row[0] * 10, backgroundColor: row[1] }}>
-                {row[0]}
-              </p>
-            </>
-          );
-        });
-      })}
+    <div className="clock-container">
+      {results.flat().map((item, index) => (
+        <div key={index} className={`clock-square ${item.color}`}>
+          {item.value}
+        </div>
+      ))}
     </div>
   );
 };
