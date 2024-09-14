4149c5516181575ddb66209896211cdc1716012b
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-14 18:00:18 +0100

    almost Completing The App ( coloring err ) Complted Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import {
-  Clock,
   OneSquare,
   ThreeSquare,
   TwoSquare,
   FiveSquare,
 } from "../src/components";
 
-const fibonacciNumbers = [1, 1, 2, 3, 5];
-
-const findColors = (arr1, arr2) => {
-  const result = [];
-
-  const countArr1 = arr1.reduce((acc, num) => {
-    acc[num] = (acc[num] || 0) + 1;
-    return acc;
-  }, {});
-  const countArr2 = arr2.reduce((acc, num) => {
-    acc[num] = (acc[num] || 0) + 1;
-    return acc;
-  }, {});
-
-  fibonacciNumbers.forEach((num) => {
-    const isInArr1 = countArr1[num] || 0;
-    const isInArr2 = countArr2[num] || 0;
-
-    if (isInArr1 > 0 && isInArr2 > 0) {
-      result.push({ value: num, color: "blue" });
-    } else if (isInArr1 > 0) {
-      result.push({ value: num, color: "red" });
-    } else if (isInArr2 > 0) {
-      result.push({ value: num, color: "green" });
-    } else {
-      result.push({ value: num, color: "white" });
-    }
-  });
-
-  return result;
-};
-
-const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-const hourCombinations = [
-  [1, 1],
-  [2, 3],
-  [1, 5],
-  [2, , 3],
-];
-
-const minuteCombinations = [[1, 2, 3, 5], [5, 2, 1], [1, 2, 5], [1]];
-
 const App = () => {
-  const hourComb = getRandomElement(hourCombinations);
-  const minuteComo = getRandomElement(minuteCombinations);
+  // Current Date
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const Hours =
+    currentTime.getHours() > 12
+      ? currentTime.getHours() % 12
+      : currentTime.getHours();
 
+  const Minutes = currentTime.getMinutes();
+
+  console.log(Hours, Math.trunc(Minutes / 5));
+
+  // fibonacci first 5 numbers.
+  const fibonacciNumbers = [1, 1, 2, 3, 5];
+
+  const ChangeTime = (num) => {
+    const updatedTime = new Date(currentTime);
+    updatedTime.setMinutes(updatedTime.getMinutes() + num);
+    setCurrentTime(updatedTime);
+  };
+
+  // find all possible combinations
+  const findAllCombinations = (fibNumbers, target) => {
+    const results = [];
+
+    const findCombinations = (index, currentSum, currentCombination) => {
+      if (currentSum === target) {
+        results.push(currentCombination.slice());
+        return;
+      }
+
+      if (currentSum > target || index >= fibNumbers.length) {
+        return;
+      }
+
+      findCombinations(index + 1, currentSum + fibNumbers[index], [
+        ...currentCombination,
+        fibNumbers[index],
+      ]);
+
+      findCombinations(index + 1, currentSum, currentCombination);
+    };
+
+    findCombinations(0, 0, [], 0);
+    return results;
+  };
+
+  // finding the colors based on the comparing numbers
+  const findColors = (arr1, arr2) => {
+    const result = [];
+
+    const countArr1 = arr1.reduce((acc, num) => {
+      acc[num] = (acc[num] || 0) + 1;
+      return acc;
+    }, {});
+    const countArr2 = arr2.reduce((acc, num) => {
+      acc[num] = (acc[num] || 0) + 1;
+      return acc;
+    }, {});
+
+    fibonacciNumbers.forEach((num) => {
+      const isInArr1 = countArr1[num] || 0;
+      const isInArr2 = countArr2[num] || 0;
+
+      if (isInArr1 > 0 && isInArr2 > 0) {
+        result.push({ value: num, color: "blue" });
+      } else if (isInArr1 > 0) {
+        result.push({ value: num, color: "red" });
+      } else if (isInArr2 > 0) {
+        result.push({ value: num, color: "green" });
+      } else {
+        result.push({ value: num, color: "white" });
+      }
+    });
+
+    return result;
+  };
+
+  // getting random variant from all variants.
+  // ----------------------------------------------------------------------
+  const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+  const hourComb = getRandomElement(
+    findAllCombinations(fibonacciNumbers, Hours)
+  );
+  const minuteComo = getRandomElement(
+    findAllCombinations(fibonacciNumbers, Math.trunc(Minutes / 5))
+  );
+
+  // the Final Results :
   const result = findColors(hourComb, minuteComo);
   console.log(result);
 
   return (
-    <div className="clock-container">
+    <div className="container">
       <div>
-        <div style={{ display: "flex" }}>
-          <TwoSquare Color={result[2].color} />
-          <div>
-            <OneSquare Color={result[0].color} />
-            <OneSquare Color={result[1].color} />
-          </div>
+        <h1>Current Time</h1>
+        <p>
+          <span style={{ marginRight: "5px" }}>
+            {currentTime.getHours() > 12
+              ? (currentTime.getHours() % 12) + "PM"
+              : currentTime.getHours() + " AM"}
+          </span>
+          {Minutes}
+        </p>
+        <div className="btns">
+          <button onClick={() => ChangeTime(-5)}>Back</button>
+          <button onClick={() => ChangeTime(5)}>Forward</button>
         </div>
-        <ThreeSquare Color={result[3].color} />
       </div>
-      <FiveSquare Color={result[4].color} />
+      <div className="clock-container">
+        <div>
+          <div style={{ display: "flex" }}>
+            <TwoSquare Color={result[2].color} />
+            <div>
+              <OneSquare Color={result[0].color} />
+              <OneSquare Color={result[1].color} />
+            </div>
+          </div>
+          <ThreeSquare Color={result[3].color} />
+        </div>
+        <FiveSquare Color={result[4].color} />
+      </div>
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
-import Clock from "./Clock";
 import OneSquare from "./Squares/OneSquare";
 import TwoSquare from "./Squares/TwoSquare";
 import ThreeSquare from "./Squares/ThreeSquare";
 import FiveSquare from "./Squares/FiveSquare";
 
-export { Clock, OneSquare, TwoSquare, ThreeSquare, FiveSquare };
+export { OneSquare, TwoSquare, ThreeSquare, FiveSquare };
